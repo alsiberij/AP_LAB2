6717d0761800b20e4f6e30b255f8751f7c3870e3
alsiberij <alsiberij@gmail.com>
2020-11-11 16:27:02 +0300

    Initial commit of CalculatorFrame class

---
+++ src/root/CalculatorFrame.java
@@ -0,0 +1,12 @@
+package root;
+
+import javax.swing.*;
+
+public class CalculatorFrame extends JFrame {
+
+    private enum Formula {FORMULA1, FORMULA2}
+
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+    
+}
