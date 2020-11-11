0598f8f30082da4fd43b19dc54fe8ecc54863eb7
alsiberij <alsiberij@gmail.com>
2020-11-11 16:34:10 +0300

    Class fields added

--- src/root/CalculatorFrame.java
+++ src/root/CalculatorFrame.java
@@ -8,5 +8,21 @@ public class CalculatorFrame extends JFrame {
 
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
-    
+
+    private Formula currentFormula = Formula.FORMULA1;
+
+    private Box boxContent = Box.createHorizontalBox();
+
+    private JLabel imgFormula1;
+    private JLabel imgFormula2;
+
+    private JTextField fieldX;
+    private JTextField fieldY;
+    private JTextField fieldZ;
+    private JTextField fieldResult;
+
+    double[] memory = new double[3];
+    private JLabel[] labelsMemory = new JLabel[3];
+    short currentMemorySection = 0;
+
 }
